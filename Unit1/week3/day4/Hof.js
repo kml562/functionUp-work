4039bee8006ad0f4b3eafbd5afab048dda3297e7
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-04-08 01:05:14 +0530

    done

--- Unit1/week3/day4/Hof.js
+++ Unit1/week3/day4/Hof.js
@@ -1,7 +1,7 @@
 // filter functin ; return array, in this array only true condition el will be filtered, if all condition false
 //  return [],
 // myfilter(trialarr,(el) => el%2==0)
-
+// function {  }
 function myfilter(arr,cond){
    let ans=[];
    for(let el of arr){
@@ -9,10 +9,30 @@ function myfilter(arr,cond){
         ans.push(el);
     }
    }
+
 return ans;
 }
+// el index,array
 
 let trialarr= [2,4,5,6,7,8,9,10,11,12,13,14];
+let xx=trialarr.filter((el)=>{
+    if(el%2===0){
+        return el;
+    }
+})
+// console.log(xx)
+// // let ansar= myfilter(trialarr,(el)=>el%2==0)
+// console.log(ansar);
 
-let ansar= myfilter(trialarr,(el)=>el%2==0)
-console.log(ansar)
+function mymap(arr,fun){
+let ans=[];
+for(let el of arr){
+ans.push(fun(el));
+}
+return ans;
+}
+const newwww=()=>{
+
+}
+let newww=mymap(trialarr,(el)=>el*2)
+console.log(newww)
