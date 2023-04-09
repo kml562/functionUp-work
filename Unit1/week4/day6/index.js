f57d1ff05ffff1e8661dd947e607465e5c24f7ab
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-04-09 23:46:14 +0530

    done

---
+++ Unit1/week4/day6/index.js
@@ -0,0 +1,71 @@
+const facebookProfiles = [
+    {
+        firstName: "Akash",
+        lastName: "Agarwal",
+        number: "111111111",
+        likes: ["music", "movies"],
+        hasDrivingLicense: false,
+        address: {
+            location: "rampur",
+            state: "up",
+        },
+        emails: ["abc@outlook.com", "efg@gamil.com", "ghj@gmail.com"],
+    },
+    {
+        firstName: "Pritesh",
+        lastName: "Kumar",
+        number: "222222222",
+        likes: ["code", "driving"],
+        hasDrivingLicense: false,
+        address: {
+            location: "gurgaon",
+            state: "haryana",
+        },
+        emails: ["fgdfg@gmail.com"],
+    },
+    {
+        firstName: "Sabiha",
+        lastName: "Khan",
+        number: "333333333",
+        hasDrivingLicense: false,
+        address: {
+            location: "lucknow",
+            state: "up",
+        },
+    },
+    {
+        firstName: "Suyash",
+        lastName: "Kashyap",
+        number: "444444444",
+        likes: ["travel", "driving"],
+        hasDrivingLicense: true,
+        address: {
+            location: "alwar",
+            state: "rajasthan",
+        },
+        emails: ["abc@yahoo.com"],
+    },
+    {
+        firstName: "Jay",
+        likes: ["food", "mobile"],
+        hasDrivingLicense: true,
+        address: {
+            location: "gurgaon",
+            state: "haryana",
+        },
+        emails: ["abc@gmail.com", "efg@yahoo.com", "ghj@outlook.com"],
+    },
+];
+
+function gurgaon(facebookProfiles){
+return facebookProfiles.reduce((acc,curr)=>{
+    let {firstName,lastName}=curr;
+    
+if(curr.address.location==="gurgaon"){
+    acc.unshift(lastName?firstName+" "+lastName:firstName)
+}
+return acc
+},[])
+
+}
+console.log(gurgaon(facebookProfiles))
